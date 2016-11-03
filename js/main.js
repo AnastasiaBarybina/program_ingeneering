8774625a31690361fa2b8a5d9562e692d69962d0
Anastasia Barybina <gerwey@mail.ru>
2016-11-03 18:15:29 +0300

    last
    Signed-off-by: Anastasia Barybina <gerwey@mail.ru>

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
-        anchors:['slide1', 'slide2', 'slide3', 'slide4', 'slide5', 'slide6', 'slide7'],
-        sectionsColor: ['#f8a109', '#4BBFC3', '#7BAABE', '#01c86d', '#f8a109', '#4BBFC3', '#7BAABE'],
+        anchors:['slide1', 'slide2', 'slide3', 'slide4', 'slide5', 'slide6', 'slide7', 'slide8' ],
+        sectionsColor: ['#f8a109', '#4BBFC3', '#7BAABE', '#01c86d', '#f8a109', '#4BBFC3', '#7BAABE', '#f8a109'],
     });
 
     testill = new function(){
